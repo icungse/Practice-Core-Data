d1783194fedca4ef96c98c498e2751867980cf4b
Saiful <icungse@gmail.com>
2019-05-09 02:42:04 +0700

    fetch from core data

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -21,6 +21,24 @@ class ViewController: UIViewController {
         title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error) \(error.userInfo)")
+        }
+    }
 
     // Implement the addName IBAction
     @IBAction func addName(_ sender: UIBarButtonItem) {
